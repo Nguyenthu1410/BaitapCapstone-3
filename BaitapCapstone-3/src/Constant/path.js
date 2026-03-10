b5b7fe636af88273faf17a8117ca6addb67c7f60
Nguyenthu1410 <nguyenngocanhthu14102007@gmail.com>
2026-03-10 09:46:02 +0700

    update lan 1

---
+++ BaitapCapstone-3/src/Constant/path.js
@@ -0,0 +1,12 @@
+export const PUBLIC_PATH = {
+    HOME: '/trang-chu',
+    SIGN_IN:'/lich-chieu',
+    SCHEDULE: '/lich-chieu',
+    CINEMA: '/rap-chieu',
+    PROMOTION: '/khuyen-mai',
+    APP:'/ung-dung',
+    SUPPORT: '/ho-tro'
+
+}
+
+export const PRIVATE_PATH = {}
